b0f108e2b149cf7c149258774ae6cff34939f660
Javier Moreno <javiomoreno@gmail.com>
2015-11-26 08:46:28 -0430

    Agregar Calendario

--- app/js/app.js
+++ app/js/app.js
@@ -6,11 +6,35 @@
     'calendario.directives',
     'calendario.services',
     'LocalStorageModule'
+
   ]);
 
 app.config(['localStorageServiceProvider', function(localStorageServiceProvider){
   localStorageServiceProvider.setPrefix('ls');
 }]);
+
+app.config(function(calendarConfigProvider) {
+
+  calendarConfigProvider.setDateFormatter('moment'); // use either moment or angular to format dates on the calendar. Default angular. Setting this will override any date formats you have already set.
+
+  calendarConfigProvider.setDateFormats({
+    hour: 'HH:mm' // this will configure times on the day view to display in 24 hour format rather than the default of 12 hour
+  });
+
+  calendarConfigProvider.setTitleFormats({
+    day: 'ddd D MMM' //this will configure the day view title to be shorter
+  });
+
+  calendarConfigProvider.setI18nStrings({
+    eventsLabel: 'Events', //This will set the events label on the day view
+    timeLabel: 'Time' //This will set the time label on the time view
+  });
+
+  calendarConfigProvider.setDisplayAllMonthEvents(true); //This will display all events on a month view even if they're not in the current month. Default false.
+
+  calendarConfigProvider.setDisplayEventEndTimes(true); //This will display event end times on the month and year views. Default false.
+
+});
 
   app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,10 +1,15 @@
 (function () {
 
-  angular.module('calendario.controllers', ['ui.bootstrap', 'ui.grid', 'ui.grid.selection', 'bootstrap.fileField'])
+    angular.module('calendario.controllers', ['ui.bootstrap', 'ui.grid', 'ui.grid.selection', 'bootstrap.fileField', 'mwl.calendar'])
       .controller('LoginController', function($scope){
 
       })
-      .controller('CalendarioController', function ($scope, $routeParams, calendarioService) {
+      .controller('CalendarioController', function ($scope, $routeParams, calendarioService, moment) {
+
+          var vm = this;
+
+          $scope.calendarView = 'month';
+          $scope.calendarDay = new Date();
 
           var mes = $routeParams.mes;
           $scope.mes = mes;
@@ -16,7 +21,24 @@
 
           $scope.getRandomImagen = function(){
               return Math.floor((Math.random()*3)+1);
-          }
+          };
+
+          $scope.events = [
+              {
+                  title: 'My event title', // The title of the event
+                  type: 'info', // The type of the event (determines its color). Can be important, warning, info, inverse, success or special
+                  startsAt: new Date(2013,5,1,1), // A javascript date object for when the event starts
+                  endsAt: new Date(2014,8,26,15), // Optional - a javascript date object for when the event ends
+                  editable: false, // If edit-event-html is set and this field is explicitly set to false then dont make it editable.
+                  deletable: false, // If delete-event-html is set and this field is explicitly set to false then dont make it deleteable
+                  draggable: true, //Allow an event to be dragged and dropped
+                  resizable: true, //Allow an event to be resizable
+                  incrementsBadgeTotal: true, //If set to false then will not count towards the badge total amount on the month and year view
+                  recursOn: 'year', // If set the event will recur on the given period. Valid values are year or month
+                  cssClass: 'a-css-class-name' //A CSS class (or more, just separate with spaces) that will be added to the event when it is displayed on each view. Useful for marking an event as selected / active etc
+              }
+          ];
+
       })
       .controller('AdministradorController', function () {
 
